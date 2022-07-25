b55340a47deb53c01c2dc8f66118327167ee7e6d
toydotgame <toydotgame@gmail.com>
2022-07-26 09:52:35 +1000

    Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody playerRigidbody;
+    private float movementX;
+    private float movementY;
+    private float movementSpeed = 10;
+
+    void OnMove(InputValue movementValue) {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate() {
+        playerRigidbody.AddForce(new Vector3(movementX, 0f, movementY) * movementSpeed);
+    }
+}

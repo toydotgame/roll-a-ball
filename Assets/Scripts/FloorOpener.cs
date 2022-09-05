fa3ea8ff7f869f9bad403014b3efd175780e90df
toydotgame <toydotgame@gmail.com>
2022-09-05 21:53:00 +1000

    Added correct checks in order for floor to not rotate more than 90°

--- Assets/Scripts/FloorOpener.cs
+++ Assets/Scripts/FloorOpener.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class FloorOpener : MonoBehaviour {
 	public GameObject leftFloor;
 	public GameObject rightFloor;
-	private float rotationSpeed = 0.001f;
+	private float rotationSpeed = 0.1f;
 
 	private void Update() {
 		if(PlayerController.winState) {
-			for(int i = 0; i < 90; i++) { 
+			if(rightFloor.transform.rotation.eulerAngles.z < 90) {
 				leftFloor.transform.Rotate(0, 0, -1 * rotationSpeed);
 				rightFloor.transform.Rotate(0, 0, 1 * rotationSpeed);
 			}
